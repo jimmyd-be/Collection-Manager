23006a2f079f0e9290b7d553c7c6dc4640660ae6
Jimmyd <dom.jimmy@gmail.com>
2019-05-31 14:39:18 +0200

    Fixed error

--- src/app/Services/custom-field.service.ts
+++ src/app/Services/custom-field.service.ts
@@ -24,16 +24,16 @@ export class CustomFieldService {
 
   getFieldsByCollection(collectionId: Number): CustomField[]{
 
-    return [new CustomField("Name", "text", [], true, "Give name of the item", 0, "left", false, "left", "Name", ""),
-    new CustomField("Type", "dropdown", ['Movie', 'old movie'], true, "Give name of the item", 0, "left", false, "left", "Type", ""),
-    new CustomField("Original", "checkBox", [], false, "Is original movie?", 0, "left", false, "left", "Original", ""),
-    new CustomField("Radio", "radio", ['Yes', 'No', 'Maybe'], true, "is this a good movie?", 0, "left", false, "left", "Good movie?", ""),
-    new CustomField("url", "url", [], true, "Give url of the trailer", 0, "main", false, "left", "Trailer", ""),
-    new CustomField("note", "textarea", [], true, "Note of the movie", 0, "main", false, "left", "Note", ""),
-    new CustomField("Mail", "email", [], true, "Note of the movie", 0, "main", false, "left", "Mail To", ""),
-    new CustomField("rating", "rate", [], true, "Note of the movie", 0, "main", false, "left", "rate movie", ""),
-    new CustomField("cover", "image", [], true, "Note of the movie", 0, "main", false, "left", "cover", ""),
-    new CustomField("ReleaseDate", "date", [], true, "Note of the movie", 0, "main", false, "left", "Release date", "")
+    return [new CustomField(5, "Name", "text", [], true, "Give name of the item", 0, "left", false, "left", "Name", ""),
+    new CustomField(5, "Type", "dropdown", ['Movie', 'old movie'], true, "Give name of the item", 0, "left", false, "left", "Type", ""),
+    new CustomField(5, "Original", "checkBox", [], false, "Is original movie?", 0, "left", false, "left", "Original", ""),
+    new CustomField(5, "Radio", "radio", ['Yes', 'No', 'Maybe'], true, "is this a good movie?", 0, "left", false, "left", "Good movie?", ""),
+    new CustomField(5, "url", "url", [], true, "Give url of the trailer", 0, "main", false, "left", "Trailer", ""),
+    new CustomField(5, "note", "textarea", [], true, "Note of the movie", 0, "main", false, "left", "Note", ""),
+    new CustomField(5, "Mail", "email", [], true, "Note of the movie", 0, "main", false, "left", "Mail To", ""),
+    new CustomField(5, "rating", "rate", [], true, "Note of the movie", 0, "main", false, "left", "rate movie", ""),
+    new CustomField(5, "cover", "image", [], true, "Note of the movie", 0, "main", false, "left", "cover", ""),
+    new CustomField(5, "ReleaseDate", "date", [], true, "Note of the movie", 0, "main", false, "left", "Release date", "")
   ];
   }
 }
