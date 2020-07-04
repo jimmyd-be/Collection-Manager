4da52f459ee744b24b64a4517d850c65a659d446
Jimmy <dom.jimmy@gmail.com>
2020-07-04 16:12:38 +0200

    Create docker-compose file to generate all containers for application

--- api/database/database.sql
+++ api/database/database.sql
@@ -100,8 +100,9 @@ CREATE TABLE field (
 CREATE TABLE itemdata (
     itemId BIGINT UNSIGNED NOT NULL,
     fieldId BIGINT UNSIGNED NOT NULL,
-    fieldValue VARCHAR(1024),
-    PRIMARY KEY (itemId, fieldId, fieldValue),
+    valueCount BIGINT UNSIGNED NOT NULL,
+    fieldValue TEXT,
+    PRIMARY KEY (itemId, fieldId, valueCount),
     FOREIGN KEY (itemId) REFERENCES item(id),
     FOREIGN KEY (fieldId) REFERENCES field(id)
 );
@@ -115,6 +116,43 @@ CREATE TABLE collectionfield(
 );
 
 
+INSERT INTO collectiontype (`id`, `type`, `active`) VALUES
+(1, 'Movies', 1),
+(2, 'Games', 1);
+
+
+INSERT INTO fieldtype (id, `type`,active) VALUES 
+(1, 'text',1)
+,(2, 'url',1)
+,(3, 'textarea',1)
+,(4, 'date',1)
+,(5, 'dropdown',1)
+,(6, 'checkbox',1)
+,(7, 'radio',1)
+,(8, 'email',1)
+,(9, 'rate',1)
+,(10, 'image',1)
+,(11, 'barcode',1),
+(12, 'number', 1);
+
+INSERT INTO newcm.`role` (id, role, active) VALUES
+(0, 'Owner', 1),
+(1, 'Admin', 1),
+(2, 'Editor', 1),
+(3, 'Viewer', 1);
 
 
 
+INSERT INTO field
+(`name`, `type`, `choises`, `required`, `placeHolder`, `label`, `otherOptions`, `collectionBaseType`, `fieldOrder`, `place`, `multiValues`, 
+`active`, `labelPosition`, `widget`) VALUES
+('title', 1, NULL, 1, 'Title', 'Title', NULL, 1, 0, 'top', 0, 1, 'hidden', 'default'),
+('genre', 1, NULL, 0, 'Genre', 'Genre', NULL, 1, 10, 'main', 1, 1, 'left', 'default'),
+('runtime', 12, NULL, 0, '', 'Runtime', NULL, 1, 20, 'main', 0, 1, 'left', 'default'),
+('releaseDate', 4, NULL, 0, 'Release Date', 'Release Date', NULL, 1, 30, 'main', 0, 1, 'left', 'default'),
+('storyline', 3, NULL, 0, 'Storyline', 'Storyline', NULL, 1, 40, 'main', 0, 1, 'top', 'default'),
+('director', 1, NULL, 0, 'Director', 'Director', NULL, 1, 50, 'main', 1, 1, 'top', 'default'),
+('writer', 1, NULL, 0, 'Writer', 'Writer', NULL, 1, 60, 'main', 1, 1, 'top', 'default'),
+('cast', 1, NULL, 0, 'Cast', 'Cast', NULL, 1, 60, 'main', 1, 1, 'top', 'default'),
+('cover', 10, NULL, 0, 'Cover', 'Cover', NULL, 1, 70, 'left', 1, 0, 'hidden', 'image'),
+('rating', 9, NULL, 0, 'Rating', 'Rating', NULL, 1, 80, 'left', 1, 0, 'hidden', 'rate');
