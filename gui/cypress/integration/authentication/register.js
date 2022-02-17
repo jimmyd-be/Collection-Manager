aaa87ae78a728178c712695e355038443ee35a5e
jimmyd-be <dom.jimmy@gmail.com>
2022-02-17 18:46:05 +0100

    #21: - Fix Cypress tests

--- gui/cypress/integration/authentication/register.js
+++ gui/cypress/integration/authentication/register.js
@@ -1,31 +1,25 @@
 describe('Authentication', () => {
   it('Visits the registration page and register a new user', () => {
-    cy.intercept('POST', 'http://localhost:8080/api/auth/register', {
-      statusCode: 200,
-    }).as('registerApi')
+    cy.intercept('POST', 'http://localhost:4200/api/auth/register').as('registerApi')
 
-    //
-    cy.visit('/auth/login')
-    cy.get('a[href*="/auth/register"]').click()
+    cy.visit('/auth/register')
     cy.get('#title').contains("Register")
-    cy.get('[id=input-name]').type("Jeff Nijs")
-    cy.get('[id=input-email]').type("test@test.be")
+    cy.get('[id=input-name]').type("Jeff Nijsd")
+    cy.get('[id=input-email]').type("test@testf.be")
     cy.get('[id=input-password]').type("SecurePassword2022!")
     cy.get('[id=input-re-password]').type("SecurePassword2022!")
     cy.get('.custom-checkbox').click()
     cy.get('.appearance-filled').click()
-    cy.wait('@registerApi')
+    cy.wait('@registerApi').then((interception) => {
+      assert.equal(interception.response.statusCode, 200)
+    })
+    cy.wait(500)
     cy.url().should('eq', 'http://localhost:4200/auth/login')
   })
 
   it('Visits the registration page and register a faulty user', () => {
-    cy.intercept('POST', 'http://localhost:8080/api/auth/register', {
-      statusCode: 500,
-    }).as('registerApi')
-
-    //
-    cy.visit('/auth/login')
-    cy.get('a[href*="/auth/register"]').click()
+    cy.intercept('POST', 'http://localhost:4200/api/auth/register').as('registerApi')
+    cy.visit('/auth/register')
     cy.get('#title').contains("Register")
     cy.get('[id=input-name]').type("Jeff Nijs")
     cy.get('[id=input-email]').type("test@test.be")
@@ -33,7 +27,9 @@ describe('Authentication', () => {
     cy.get('[id=input-re-password]').type("OtherPassword!")
     cy.get('.custom-checkbox').click()
     cy.get('.appearance-filled').click()
-    cy.wait('@registerApi')
+    cy.wait('@registerApi').then((interception) => {
+      assert.equal(interception.response.statusCode, 409)
+    })
     cy.get('.outline-danger').contains('Oh snap!')
   })
 })
